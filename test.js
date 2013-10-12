6f7a1843b756a9e2a6b31976fecdc8e98f13b759
Rod Vagg <rod@vagg.org>
2013-10-12 20:33:43 +1100

    fix tests, no destroy or repair

--- test.js
+++ test.js
@@ -1,1 +1,5 @@
-require('level-packager/test')(require('tape'), require('./'))
+require('level-packager/test')(
+    require('tape')
+  , require('./')
+  , { skipRepairTest: true, skipDestroyTest: true }
+)
